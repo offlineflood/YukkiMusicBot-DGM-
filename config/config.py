13a219dc5662da87832d4353aabaf2f5d18828cc
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2023-01-10 19:14:27 +0400

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001718026761"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "QTX Music Bot")
 
 # Your User ID.
 OWNER_ID = list(
